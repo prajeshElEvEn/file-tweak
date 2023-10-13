b012a24bb8b971efd1887d68510d16bfa6d1dc1f
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-10-14 02:03:59 +0530

    removed notebooks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
+from termcolor import colored
+from pyfiglet import figlet_format
 from scripts import textToCSV
 
 input_file_path = os.path.join('data', 'input')
@@ -6,5 +8,13 @@ output_file_path = os.path.join('data', 'output')
 input_file_name = "data.txt"
 output_file_name = "data.csv"
 
+ascii_art = figlet_format("file-tweak", font="slant")
+colored_ascii_art = colored(ascii_art, color='blue')
+print(colored_ascii_art)
+print(colored("by @eleven", color='grey'))
+print(colored("----------------------------------------------------", color='grey'))
+
 textToCSV.textToCSV(input_file_path, input_file_name,
                     output_file_path, output_file_name)
+
+print(colored("----------------------------------------------------", color='grey'))
--- scripts/textToCSV.py
+++ scripts/textToCSV.py
@@ -1,18 +1,31 @@
 import os
 import csv
 from tqdm import tqdm
+from termcolor import colored
 
 
 def textToCSV(input_file_path, input_file_name, output_file_path, output_file_name):
-    total_lines = sum(1 for line in open(
-        os.path.join(input_file_path, input_file_name)))
+    try:
+        input_file = os.path.join(input_file_path, input_file_name)
+        output_file = os.path.join(output_file_path, output_file_name)
+        print(colored(f"> Converting {input_file} to {output_file}", "green"))
+        total_lines = sum(1 for line in open(input_file))
 
-    with open(os.path.join(input_file_path, input_file_name), 'r') as txt_file, open(os.path.join(output_file_path, output_file_name), 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
+        with open(input_file, 'r') as txt_file, open(output_file, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
 
-        for line in tqdm(txt_file, total=total_lines, desc="Converting .txt to .csv"):
-            data = line.strip().split()
-            csv_writer.writerow(data)
+            for line in tqdm(txt_file, total=total_lines, desc=colored("Converting .txt to .csv", "yellow")):
+                data = line.strip().split()
+                rank = data[0]
+                name = ' '.join(data[1:-3])
+                university = data[-3]
+                roll_no = data[-2]
+                section = data[-1]
+                csv_writer.writerow([rank, name, university, roll_no, section])
 
-    print(
-        f"Conversion from {input_file_path,input_file_name} to {os.path.join(output_file_path,output_file_name)} is complete.")
+        print(colored("> Conversion complete", "green"))
+
+    except FileNotFoundError as e:
+        print(colored(f"Error: {e}", "red"))
+    except Exception as e:
+        print(colored(f"An unexpected error occurred: {e}", "red"))
